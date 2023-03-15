560f7efdf4d6d34d868f31509a38a075d4132aab
quangphe <dd>
2023-03-15 13:47:49 +0700

    update

--- Home/person_sci_1_0_0/js/person_sci_1_0_0_data.js
+++ Home/person_sci_1_0_0/js/person_sci_1_0_0_data.js
@@ -2,19 +2,19 @@ const dataPerson = [
     {
         id: "tabPerson0",
         name: "Mr. Nguyễn Bá Úy",
-        img: "Home/person_sci_1_0_0/images/person-pic1.jpg",
+        img: "Home/person_sci_1_0_0/images/person-pic1.jpg?ver2",
         sub: "Luôn nhìn vào điểm tích cực của nhân viên & <br> giúp đỡ họ tốt lên mỗi ngày",
     },
     {
         id: "tabPerson1",
         name: "Mr. Nguyễn Bá Úy1",
-        img: "Home/person_sci_1_0_0/images/person-pic1.jpg",
+        img: "Home/person_sci_1_0_0/images/person-pic1.jpg?ver2",
         sub: "Luôn nhìn vào điểm tích cực của nhân viên & <br> giúp đỡ họ tốt lên mỗi ngày",
     },
     {
         id: "tabPerson2",
         name: "Mr. Nguyễn Bá Úy2",
-        img: "Home/person_sci_1_0_0/images/person-pic1.jpg",
+        img: "Home/person_sci_1_0_0/images/person-pic1.jpg?ver2",
         sub: "Luôn nhìn vào điểm tích cực của nhân viên & <br> giúp đỡ họ tốt lên mỗi ngày",
     },
 ];
